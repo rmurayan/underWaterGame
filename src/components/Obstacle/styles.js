0cfbc0ddce009949cff989b049baf2293d710cd4
Maria Cristina Rondina <mariacristinarondina@Marias-MacBook.fc.ca>
2024-04-01 20:10:00 -0400

    Checking the obstacles' borders

--- src/components/Obstacle/styles.js
+++ src/components/Obstacle/styles.js
@@ -3,6 +3,9 @@ import { StyleSheet } from 'react-native'
 export const styles = ({ xBody, yBody, widthBody, heightBody }) =>
   StyleSheet.create({
     obstacle: {
+      borderWidth: 1,
+      borderColor: "darkBrown",
+      borderStyle: "solid",
       position: 'absolute',
       left: xBody,
       margin :0,
