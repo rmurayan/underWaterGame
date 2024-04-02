fc3040d1e77b79f5b2fc467173684b424157340f
Maria Cristina Rondina <mariacristinarondina@Marias-MacBook.fc.ca>
2024-04-01 20:07:59 -0400

    Checking the borders on each objects

--- src/components/Fish/styles.js
+++ src/components/Fish/styles.js
@@ -3,6 +3,9 @@ import { StyleSheet } from 'react-native'
 export const styles = ({ xBody, yBody, widthBody, heightBody }) =>
   StyleSheet.create({
     fish: {
+      borderWidth: 1,
+      borderColor: "darkBrown",
+      borderStyle: "solid",
       position: 'absolute',
       left: xBody,
       top: yBody,
--- src/components/Floor/styles.js
+++ src/components/Floor/styles.js
@@ -3,6 +3,10 @@ import { StyleSheet } from 'react-native'
 export const styles = ({ xBody, yBody, widthBody, heightBody }) =>
   StyleSheet.create({
     floor: {
+      borderWidth: 1,
+      borderColor: "darkBrown",
+      borderStyle: "solid",
+      position: 'absolute',
       position: 'absolute',
       left: xBody,
       top: yBody,
